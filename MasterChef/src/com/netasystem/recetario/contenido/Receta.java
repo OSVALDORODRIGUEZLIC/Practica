0e0bc61a71ecda1e92e4a59d96633f69dba886fc
OSVALDORODRIGUEZLIC <42583740+OSVALDORODRIGUEZLIC@users.noreply.github.com>
2018-08-22 16:03:16 -0500

    receta2

--- MasterChef/src/com/netasystem/recetario/contenido/Receta.java
+++ MasterChef/src/com/netasystem/recetario/contenido/Receta.java
@@ -3,7 +3,6 @@ package com.netasystem.recetario.contenido;
 import java.util.List;
 
 public class Receta {
-
 	public Receta(String nombrechef, String nombrereceta, List listamateriales) {
 		super();
 		this.nombrechef = nombrechef;
