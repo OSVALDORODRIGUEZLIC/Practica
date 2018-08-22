cbffaffea442bee583fa820a9116f1033ae13d64
OSVALDORODRIGUEZLIC <42583740+OSVALDORODRIGUEZLIC@users.noreply.github.com>
2018-08-22 14:50:36 -0500

    asd

---
+++ Constructor/src/coche.java
@@ -0,0 +1,38 @@
+
+public class coche {
+
+	private int velocidad, aceleracion;
+	private String nombre;
+	
+	//Set and Get
+	
+	public int getVelocidad() {
+		return velocidad;
+	}
+	public void setVelocidad(int velocidad) {
+		this.velocidad = velocidad;
+	}
+	public int getAceleracion() {
+		return aceleracion;
+	}
+	public void setAceleracion(int aceleracion) {
+		this.aceleracion = aceleracion;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	//metodo
+	public void acelearar (int aceleracion) {
+		this.aceleracion= aceleracion;
+		velocidad += this.aceleracion;
+	}
+	
+	public void frenar () {
+		velocidad = 0;
+	}
+	
+}
---
+++ Constructor/src/main.java
@@ -0,0 +1,14 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		
+	
+	coche c = new coche();
+	
+	c.setAceleracion(0);
+	
+	
+	
+}	
+}
